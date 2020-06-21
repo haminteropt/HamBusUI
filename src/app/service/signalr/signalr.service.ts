9e868541d2ccb59b2e9ca0cd0f8ec32580393dbc
Darryl Wagoner <darryl.wagoner@statellite.com>
2020-06-21 11:39:55 -0500

    login is working

--- src/app/service/signalr/signalr.service.ts
+++ src/app/service/signalr/signalr.service.ts
@@ -11,6 +11,24 @@ export class SignalrService {
 
 
   constructor() {
+    this.startConnection();
+
+  }
+
+  public loginResponse = (resp): void => {
+    console.log(resp);
+
+  }
+
+  private login(): void {
+    const groupList: Array<string> = [];
+    groupList.push('ui');
+    this.connection.invoke('Login', 'UI', groupList).catch((err) => {
+      console.log(err.toString());
+    });
+  }
+
+  private startConnection = () => {
     this.connection = new signalR.HubConnectionBuilder()
       .configureLogging(signalR.LogLevel.Information)
       .withUrl('http://localhost:7300/masterbus')
@@ -19,19 +37,11 @@ export class SignalrService {
 
     this.connection.start().then(() => {
       console.log('Connected!');
+      this.connection.on('ReceiveConfiguration', this.loginResponse);
+      this.login();
     }).catch((err) => {
       console.error(err.toString());
       return;
     });
-
-    // this.connection.on('BroadcastMessage', this.broadcastMessage);
-    this.connection.on('LoginResponse', this.loginResponse);
-
   }
-
-  public loginResponse = (resp): void => {
-    console.log(resp);
-  }
-
-
 }
