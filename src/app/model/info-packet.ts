40921b745c751ceaa9ca1ead6777a5c4bc00b099
Darryl Wagoner <darryl.wagoner@statellite.com>
2020-06-26 17:35:58 -0500

    Check point

--- src/app/model/info-packet.ts
+++ src/app/model/info-packet.ts
@@ -3,4 +3,5 @@ import { ActiveBuses } from './activeBuses';
 export class InfoPacket {
     public activeBuses: ActiveBuses[] = [];
     public busesInDB: BusConfigurationDb[] = [];
+    public ports: Array<string> = [];
 }
