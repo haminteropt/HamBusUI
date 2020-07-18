52151934e572afdb640c7377e74fff38c401772f
Darryl Wagoner <darryl.wagoner@statellite.com>
2020-07-18 14:44:16 -0500

    added start of dash board and able to set styles in code

---
+++ BlazorBus/BlazorBus/BlazorBus/Components/DashBoard.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BlazorBus.Services;
+using BlazorComponentUtilities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorBus.Components
+{
+  public partial class DashBoard
+  {
+    public string FirstRadio { get; set; }
+    [Inject]
+    public IHamSignalRService SigR { get; set; }
+    protected string StyleToRender;
+
+    void Change(MouseEventArgs e)
+    {
+      StyleToRender  =
+         StyleBuilder.Default("background-color", "red")
+         .AddStyle("border", "1px solid black")
+       .Build();
+
+    }
+    protected override void OnAfterRender(bool firstRender)
+    {
+
+      //Thread.Sleep(10000);
+      //FirstRadio = new CssBuilder()
+      //                    .AddClass("radio-color")
+      //                    .Build();
+
+
+    }
+
+  }
+}
