0c41b5e21693c3b19b1417cfbd21cdee63098568
Darryl Wagoner <darryl.wagoner@statellite.com>
2020-08-29 14:04:09 -0500

    attempt to use fluent validation.  Looks really cool, but not compatable with core 3.1

---
+++ BlazorBus/BlazorBus/BlazorBus/Validators/RigSettingValidator.cs
@@ -0,0 +1,15 @@
+﻿using BlazorBus.Pages;
+using FluentValidation;
+using HamBusCommonStd;
+
+namespace BlazorBus.Validators
+{
+  public class RigSettingValidator : AbstractValidator<RigConf>
+  {
+    public RigSettingValidator()
+    {
+      RuleFor(r => r.name.Length).GreaterThan(3).WithMessage("Must have name");
+      RuleFor(r => r.commPortName.Length).GreaterThan(3).WithMessage("Must have COM Port");
+    }
+  }
+}
